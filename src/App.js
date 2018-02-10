b1add4903cf17bf7ca57fc35dffa2354d66c1d74
Daniela Martin <danielamartinmtz@gmail.com>
2018-02-09 18:07:51 -0600

    bottom-tear svg added

--- src/App.js
+++ src/App.js
@@ -79,7 +79,6 @@ class App extends Component {
             <NestedList tasks={tasks} />
           </MuiThemeProvider>
         </div>
-        <TaskList tasks={tasks} />
       </div>
     );
   }
--- src/MobileTearSheet.js
+++ src/MobileTearSheet.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import bottom from "./bottom-tear.svg";
 
 class MobileTearSheet extends Component {
   static propTypes = {
@@ -29,23 +30,22 @@ class MobileTearSheet extends Component {
         border: "solid 1px #d9d9d9",
         borderBottom: "none",
         height: this.props.height,
+        width: 300,
         overflow: "hidden"
       },
       bottomTear: {
         display: "block",
         position: "relative",
-        marginTop: -10,
-        maxWidth: 360
+        marginTop: -4,
+        maxWidth: 300
       }
     };
 
     return (
       <div style={prepareStyles(styles.root)}>
         <div style={prepareStyles(styles.container)}>{this.props.children}</div>
-        <img
-          style={prepareStyles(styles.bottomTear)}
-          src="images/bottom-tear.svg"
-        />
+
+        <img style={prepareStyles(styles.bottomTear)} src={bottom} />
       </div>
     );
   }
